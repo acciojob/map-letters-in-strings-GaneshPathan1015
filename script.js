65a12331c819eef173ca372b713e6c39dfae02f1
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-29 13:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let strobj = {}
+for(let i=0;i<str.length;i++){
+    let character = str.charAt(i);
+    if(strobj[character] ===undefined){
+        strobj[character] = [i];
+    }
+    else{
+        strobj[character].push(i) ;
+    }
+}
