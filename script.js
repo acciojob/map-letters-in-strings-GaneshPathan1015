8ad544a1037b07013590219ba772890903afab13
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-29 13:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //your JS code here. If required.
-
-let strobj = {}
+let str = prompt("Ente a string");
+    str.toLowerCase();
+let strobj = {};
 for(let i=0;i<str.length;i++){
     let character = str.charAt(i);
-    if(strobj[character] ===undefined){
+    if(strobj[character] === undefined){
         strobj[character] = [i];
     }
     else{
         strobj[character].push(i) ;
     }
 }
+console.log(strobj);
